fcdcbee926157512f4f2d1a1727e3cf5c94fb51d
Aakash <rktaakash@gmail.com>
2023-11-27 02:55:14 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
-
+import path from 'path';
 const app=express();
+// setting up view engine
+app.set("view engine", "ejs")
 
-app.get("/",(req,res)=>{
-    res.sendStatus(404)
-})
+app.get("/getproducts",(req,res)=>{
+   
+     res.render("index",{name:"Aakash"});
+    });
 
 app.listen(5000,()=>{
     console.log("Server is working");
