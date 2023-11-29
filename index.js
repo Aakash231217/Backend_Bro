b3ec67ca7d5e5b6d33c743a3ba8b3f868aac5b2f
Aakash <rktaakash@gmail.com>
2023-11-30 01:07:36 +0530

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ import express from "express";
 import path from 'path';
 
 const app=express();
+const users=[];
 app.use(express.static(path.join(path.resolve(),"public")));
+app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
+
 app.get("/",(req,res)=>{
-     res.sendFile("index.html")
+     res.render("index",{name:"Singh"});
+})
+
+app.get("/success",(req,res)=>{
+    res.render("success");
+})
+
+app.post("/contact",(req,res)=>{
+   console.log(req.body);
+   users.push({username:req.body.name,email:req.body.email});
+   res.redirect("/success");
+})
+
+app.get("/users",(req,res)=>{
+    res.json({
+        users,
+    })
 })
 app.listen(5000,()=>{
     console.log("Server is working")
