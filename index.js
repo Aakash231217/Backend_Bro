68b809ffacd1e76c6ffc1fb45dd850c294c13c31
Aakash <rktaakash@gmail.com>
2023-12-02 23:52:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import express from "express";
 import path from 'path';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb://localhost:27017",{
+    dbname:"backend",
+})
+.then(()=>console.log("Database Connected"))
+.catch((e)=>console.log(e));
+
+const messageSchema= new mongoose.Schema({
+name:String,
+email:String,
+})
+
+const Message= mongoose.model("Message",messageSchema);
+
 
 const app=express();
 const users=[];
@@ -11,7 +26,11 @@ app.set("view engine","ejs");
 app.get("/",(req,res)=>{
      res.render("index",{name:"Singh"});
 })
-
+app.get("/add",(req,res)=>{
+    Message.create({name:"Aakash",email:"rktaakash@gmail.com"}).then(()=>{
+        res.send("Nice")
+    });
+})
 app.get("/success",(req,res)=>{
     res.render("success");
 })
@@ -19,7 +38,7 @@ app.get("/success",(req,res)=>{
 app.post("/contact",(req,res)=>{
    console.log(req.body);
    users.push({username:req.body.name,email:req.body.email});
-   res.redirect("/success");
+   res.redirect("/successpage");
 })
 
 app.get("/users",(req,res)=>{
